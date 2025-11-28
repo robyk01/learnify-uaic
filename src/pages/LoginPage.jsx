7ce4b5269aa712a472687d5d332f44111aedf517
Roberto <amoreroberto85@gmail.com>
2025-11-28 20:32:48 +0200

    Added redirect after login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,22 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared' 
 import { supabase } from '../supabaseClient'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const LoginPage = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const {data: {subscription}} = supabase.auth.onAuthStateChange((event, session) => {
+            if (event == 'SIGNED_IN'){
+                navigate('/')
+            }
+        })
+
+        return () => subscription.unsubscribe()
+    }, [navigate])
+
     return(
         <div className='min-h-screen bg-slate-950 flex items-center justify-center p-4'>
             <div className='max-w-md w-full bg-slate-900 rounded-xl p-8 border border-slate-800'>

9c76333e5b60339948cd75840b11facc3f086a57
Roberto <amoreroberto85@gmail.com>
2025-12-01 20:46:22 +0200

    Fix duplicate react issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   optimizeDeps: {
     exclude: ['@supabase/auth-ui-react', '@supabase/auth-ui-shared'],
+  },
+
+  resolve: {
+    dedupe: ['react', 'react-dom'],
   },
 })
